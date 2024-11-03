e8d3d62713ba11bd79997b1a1acc9e841f2143ea
dlee8870 <danielh.lee@mail.utoronto.ca>
2024-11-02 21:05:41 -0400

    fixed typos

--- my-app/pages/api/templates/[id].js
+++ my-app/pages/api/templates/[id].js
@@ -1,5 +1,5 @@
 import { PrismaClient } from '@prisma/client';
-import { authenticateUser } from '@/middlware/auth';
+import { authenticateUser } from '@/middleware/auth';
 
 const prisma = new PrismaClient();
 
